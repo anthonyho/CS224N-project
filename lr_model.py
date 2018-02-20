4bb9d649dafb652b38f9ba3bf1b115cf22e1cfc0
Anthony Ho <anthony.hyho@gmail.com>
2018-02-20 14:15:08 -0800

    add functional feed forward nn model

--- lr_model.py
+++ lr_model.py
@@ -2,7 +2,8 @@ from model import Model
 import tensorflow as tf
 import preprocessing
 
-config = {'n_features' : 50,
+config = {'n_epochs': 10,
+          'n_features' : 50,
           'n_classes'  : 2,
           'hidden_size': 20,
           'lr'         : .0005
@@ -10,32 +11,58 @@ config = {'n_features' : 50,
 
 class LogisticRegressionModel(Model):
 
-    def build(self, config=None):
-        self.config = config
-        self.input_placeholder = tf.placeholder(tf.float32,shape=(None,self.config['n_features']))
-        self.labels_placeholder = tf.placeholder(tf.float32,shape=(None,self.config['n_classes']))
+    def add_placeholders(self):
+        self.input_placeholder = tf.placeholder(tf.float32,
+                                                shape=(None, self.config['n_features']))
+        self.labels_placeholder = tf.placeholder(tf.float32,
+                                                 shape=(None, self.config['n_classes']))
 
-        # prediction op
-        W1 = tf.Variable(initial_value=tf.contribs.layers.xavier_initializer((self.config['n_features'],self.config['hidden_size']),uniform=False, seed=None, dtype=tf.float32), dtype=tf.float32)
-        b1 = tf.Variable(initial_value=tf.zeros(1,self.config['hidden_size']), dtype=tf.float32)
-        
-        h = tf.nn.relu(tf.matmul(W1,self.input_placeholder)+b1)
+    def create_feed_dict(self, inputs_batch, labels_batch=None):
+        feed_dict = {self.input_placeholder: inputs_batch}
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
+        return feed_dict
 
-        W2 = tf.Variable(initial_value=tf.contribs.layers.xavier_initializer((self.config['hidden_size'],self.config['n_classes']),uniform=False, seed=None, dtype=tf.float32), dtype=tf.float32)
-        b2 = tf.Variable(initial_value=tf.zeros(1,self.config['n_classes']), dtype=tf.float32)
-	
-        self.pred = tf.matmul(W2,h)+b2
-        # loss op
+    def add_prediction_op(self):
+        W1 = tf.get_variable('W1',
+                             shape=[self.config['n_features'], self.config['hidden_size']],
+                             initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        b1 = tf.Variable(tf.zeros((1, self.config['hidden_size'])),
+                         dtype=tf.float32)
+        h = tf.nn.relu(tf.matmul(self.input_placeholder, W1) + b1)
+        W2 = tf.get_variable('W2',
+                             shape=[self.config['hidden_size'], self.config['n_classes']],
+                             initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        b2 = tf.Variable(initial_value=tf.zeros((1, self.config['n_classes'])),
+                         dtype=tf.float32)
+        pred = tf.matmul(h, W2) + b2
+        return pred
 
-        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.labels_placeholder),logits=self.pred)
-        # training op
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.config['lr'])
-        self.train_op = optimizer.minimize(self.loss)
+    def add_loss_op(self, pred):
+        loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.labels_placeholder,
+                                                       logits=pred)
+        loss = tf.reduce_mean(loss)
+        return loss
 
-    def train(self, list_list_tokens, array_labels):
-        sess = tf.Session()
-        list_list_inds = preprocessing.token_list_to_ids(list_list_tokens,self.word2id)
-        sentence_avgs = preprocessing.average_sentence_vectors(list_list_inds,self.emb_matrix):
-        feed = {self.input_placeholder : sentence_avgs,
-                self.labels_placeholder: array_labels}
-        sess.run([self.train_op,self.loss],feed_dict=feed)
+    def add_training_op(self, loss):
+        opt = tf.train.AdamOptimizer(learning_rate=self.config['lr'])
+        train_op = opt.minimize(loss)
+        return train_op
+
+    def train_on_batch(self, sess, inputs_batch, labels_batch):
+        feed = self.create_feed_dict(inputs_batch, labels_batch=labels_batch)
+        _, loss = sess.run([self.train_op, self.loss], feed_dict=feed)
+        return loss
+
+    def run_epoch(self, sess, list_list_tokens, array_labels):
+        list_list_inds = preprocessing.token_list_to_ids(list_list_tokens, self.word2id)
+        sentence_avgs = preprocessing.average_sentence_vectors(list_list_inds, self.emb_matrix)
+        loss = self.train_on_batch(sess, sentence_avgs, array_labels)
+        return loss
+
+    def train(self, sess, list_list_tokens, array_labels):
+        list_loss = []
+        for epoch in range(self.config['n_epochs']):
+            list_loss.append(self.run_epoch(sess, list_list_tokens, array_labels))
+        return list_loss
+    
--- model.py
+++ model.py
@@ -3,11 +3,13 @@ import tensorflow as tf
 
 class Model(object):
 
-    def __init__(self, glove_dim):
+    def __init__(self, glove_dim, config=None):
         glove_prefix = 'data/glove/glove.6B.'
         glove_suffix = 'd.txt'
         glove_file = glove_prefix+str(glove_dim)+glove_suffix
         self.emb_matrix, self.word2id, self.id2word = get_glove(glove_file, glove_dim)
+        self.config = config
+        self.build()
 
     def build(self, hyperparams_dict=None):
         raise NotImplementedError()
@@ -28,3 +30,9 @@ class Model(object):
     def evaluate(self, array_true_labels, method='roc'):
         # calling self.predicted_labels
         raise NotImplementedError()
+
+    def build(self):
+        self.add_placeholders()
+        self.pred = self.add_prediction_op()
+        self.loss = self.add_loss_op(self.pred)
+        self.train_op = self.add_training_op(self.loss)
