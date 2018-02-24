fc43554f52a206ffcc5af2b3c311954f746091e1
Anthony Ho <anthony.hyho@gmail.com>
2018-02-24 01:14:17 -0800

    add print functions

--- evaluate.py
+++ evaluate.py
@@ -2,26 +2,35 @@ from sklearn.metrics import roc_curve, roc_auc_score, \
     precision_recall_curve, average_precision_score
 
 
-def evaluate(y_true, y_score,
-             metrics=['roc', 'prc'], names=None): # add plot
+metric_full = {'roc': 'ROC AUC',
+               'prc': 'average precision'}
+
+
+def evaluate_all(y_true, y_score,
+                 metrics=['roc', 'prc'], names=None, plot=False):
 
     n_columns = y_true.shape[1]
 
+    if not isinstance(metrics, list):
+        metrics = [metrics]
+
     if names is None:
-        names = ['Column '+str(i+1) for i in range(n_columns)]
+        names = ['column '+str(i+1) for i in range(n_columns)]
 
     results = {}
     for metric in metrics:
         results[metric] = {}
-        scores = compute_score(y_true, y_score, metric=metric, average=False)
+        scores = evaluate(y_true, y_score, metric=metric, average=False)
         for name, score in zip(names, scores):
             results[metric][name] = score
-        results[metric]['average'] = compute_score(y_true, y_score,
-                                                   metric=metric, average=True)
+            print "{} of {} = {:.4f}".format(metric_full[metric], name, score)
+        results[metric]['average'] = evaluate(y_true, y_score,
+                                              metric=metric, average=True)
+        print "Mean column-wise {} = {:.4f}".format(metric_full[metric], score)
     return results
 
 
-def compute_score(y_true, y_score, metric='roc', average=True):
+def evaluate(y_true, y_score, metric='roc', average=True):
     if average:
         if metric == 'roc':
             return roc_auc_score(y_true, y_score, average='macro')
@@ -32,4 +41,3 @@ def compute_score(y_true, y_score, metric='roc', average=True):
             return roc_auc_score(y_true, y_score, average=None)
         if metric == 'prc':
             return average_precision_score(y_true, y_score, average=None)
-
