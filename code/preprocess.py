cd04dd56c985a42dff899769535db0f41638c9b8
Anthony Ho <anthony.hyho@gmail.com>
2018-03-08 16:36:33 -0800

    fix <unk> to <pad>; clean up and pep8

--- code/preprocess.py
+++ code/preprocess.py
@@ -7,6 +7,8 @@ import vocab
 
 
 id_unknown = 1
+pad_token = u'<pad>'
+glove_dir = '../data/glove'
 
 
 def load_data(file_name):
@@ -26,31 +28,37 @@ def _tokenize_single_string(string):
     return tokens
 
 
-def _uniformize_list_tokens_length(list_tokens, target_length,zero_token_id=u'<UNK>'):
-    padded_list_tokens = list_tokens[:target_length] #clip if too long
+def _uniformize_comment_length(list_tokens, target_length,
+                               pad_token=pad_token):
+    padded_list_tokens = list_tokens[:target_length]  # clip if too long
     mask = len(padded_list_tokens) * [True]
     while len(padded_list_tokens) < target_length:
-        padded_list_tokens.append(zero_token_id)
+        padded_list_tokens.append(pad_token)
         mask.append(False)
     return padded_list_tokens, mask
 
-def tokenize_df(df, column='comment_text',target_length=None,**kwargs):
+
+def tokenize_df(df, column='comment_text', target_length=None, **kwargs):
     list_list_tokens = []
-    masks = []
+    if target_length is not None:
+        masks = []
     for index, item in df[column].iteritems():
         list_tokens = _tokenize_single_string(item)
         if target_length is not None:
-            padded_list_tokens, mask = _uniformize_list_tokens_length(list_tokens,target_length,**kwargs)
-            list_list_tokens.append(padded_list_tokens)
+            list_tokens, mask = _uniformize_comment_length(list_tokens,
+                                                           target_length,
+                                                           **kwargs)
+            list_list_tokens.append(list_tokens)
             masks.append(mask)
-        else: 
+        else:
             list_list_tokens.append(list_tokens)
     if target_length is not None:
         return list_list_tokens, masks
     else:
         return list_list_tokens
 
-def tokens_to_ids(list_list_tokens, word2id):
+
+def tokens_to_ids(list_list_tokens, word2id, id_unknown=id_unknown):
     return [[word2id.get(token, id_unknown) for token in list_tokens]
             for list_tokens in list_list_tokens]
 
@@ -66,9 +74,9 @@ def filter_labels(df_train, columns):
     return df_train[columns].values.astype('float32')
 
 
-def get_glove(glove_dim, glove_file=None):
+def get_glove(glove_dim, glove_file=None, glove_dir=glove_dir):
     if glove_file is None:
-        glove_prefix = '../data/glove/glove.6B.'
+        glove_prefix = os.path.join(glove_dir, 'glove.6B.')
         glove_suffix = 'd.txt'
         glove_file = glove_prefix+str(glove_dim)+glove_suffix
     return vocab.get_glove(glove_file, glove_dim)
@@ -82,7 +90,7 @@ def split_train_dev(inputs, labels, masks=None,
     Inputs:
     - inputs: list or numpy array (to be splitted across rows)
     - labels: list or numpy array (to be splitted across rows)
-    =- 
+    - masks:  list or numpy array (to be splitted across rows)
     - fraction_dev: fraction of data to be held out for dev set
     - shuffle: bool to randomly shuffle data before splitting
     '''
@@ -97,14 +105,14 @@ def split_train_dev(inputs, labels, masks=None,
         np.random.shuffle(ind)
     boundary = int((1 - fraction_dev) * n_data)
     inputs_train = utils._get_items(inputs, ind[0:boundary])
-    labels_train = utils._get_items(labels, ind[0:boundary])
-    if masks is not None:
-        masks_train = utils._get_items(masks, ind[0:boundary])
     inputs_dev = utils._get_items(inputs, ind[boundary:])
+    labels_train = utils._get_items(labels, ind[0:boundary])
     labels_dev = utils._get_items(labels, ind[boundary:])
     if masks is not None:
+        masks_train = utils._get_items(masks, ind[0:boundary])
         masks_dev = utils._get_items(masks, ind[boundary:])
-    if masks is not None:
-        return inputs_train, labels_train, masks_train, inputs_dev, labels_dev, masks_dev
+        return (inputs_train, labels_train, masks_train,
+                inputs_dev, labels_dev, masks_dev)
     else:
-        return inputs_train, labels_train, inputs_dev, labels_dev
+        return (inputs_train, labels_train,
+                inputs_dev, labels_dev)
