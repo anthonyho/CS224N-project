fix loss normalization by class weights

code/model.py
@@ -10,7 +10,7 @@ class Config(object):
             setattr(self, key, value)
         if hasattr(self, 'class_weights'):
             self.class_weights = np.array(self.class_weights)
-            self.class_weights_sum = np.sum(self.class_weights)
+            self.mean_class_weights = np.mean(self.class_weights)
 
 
 class Model(object):

code/rnn_model.py
@@ -130,7 +130,7 @@ class RNNModel(Model):
                                                        logits=pred)
         if hasattr(self.config, 'class_weights'):
             weighted_mean = tf.reduce_mean(loss * self.config.class_weights)
-            return weighted_mean / self.config.class_weights_sum
+            return weighted_mean / self.config.mean_class_weights
         else:
             return tf.reduce_mean(loss)
 